f93fcd78676bc413445e7c922006faed813b46a6
Trevor Martz <trevormartz1995@gmail.com>
2016-02-25 10:41:40 -0700

    Stubbed some methods for tokenizing

--- src/shell/source/parser.c
+++ src/shell/source/parser.c
@@ -2,20 +2,58 @@
 
 #define CURRENT_STATE = 'A'
 
+struct instruction {
+	char[] instruction_string;
+	int instruciton_index;
+};
+
 // take in a stream of characters
 void take_stream(char[] string) {
     
-    size_t sizeOfString = sizeof(string) / sizeof(string[0])
-    
-    while(true) {
-        switch(string) {
-        case 'c':
-            if(CURRENT_STATE == 'C')
-                
-        case 'd':
-            printf("does the thing");
-            break;
-        }    
-    }
-    
+    size_t size_of_string = sizeof(string) / sizeof(string[0])
+    int index = 0;
+    while(index < size_of_string)
+	{
+		char current = string[index];
+		switch(current)
+		{
+			case 'a':
+				if(CURRENT_STATE == 'A')
+				{
+					go to state as needed
+				}
+				else if...
+			case 'b':
+			case 'c':
+			case 'd':
+			case 'e':
+			...
+			case 'z':
+			case '0':
+			case '1':
+			case '2':
+			...
+			case '9':
+			case '-':
+			//any other special character
+		}
+	}
+};
+
+void try_parse_instruction(instruction ins)
+{
+	go through and try to parse sections;
+	try_parse_command(ins);
+	try_parse_param(ins);
+	take the tokens and interpret instruction
+};
+
+void try_parse_command(instruction ins)
+{
+	try to parse a command and create token;
+};
+
+void try_parse_param(instruction ins)
+{
+	try to parse 1 or more param and create token
 };
